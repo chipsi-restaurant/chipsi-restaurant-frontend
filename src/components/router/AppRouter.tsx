5154184ff87a2e9eef1f7c1e0ac46d0557d457e9
sinusxd <senya.kolyshkin.gg@gmail.com>
2025-03-22 21:04:12 +0300

    CHIPSI-24 Добавил приватные пути в роутер

--- src/components/router/AppRouter.tsx
+++ src/components/router/AppRouter.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, {JSX} from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import Login from "../pages/login/Login";
 import Signup from "../pages/signup/Signup";
+import Main from "../pages/main/Main";
+import {useSelector} from "react-redux";
+import {RootState} from "../../store/store";
+
+
+
+interface PrivateRouteProps {
+    children: JSX.Element;
+}
+
+interface PrivateRouteProps {
+    children: JSX.Element;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+    const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return children;
+};
 
 const AppRouter = () => {
     return (
         <Routes>
+            <Route
+                path="/"
+                element={
+                    <PrivateRoute>
+                        <Main />
+                    </PrivateRoute>
+                }
+            />
             <Route path="/login" element={<Login/>} />
-            <Route path="/signin" element={<Signup/>}/>
+            <Route path="/signup" element={<Signup/>}/>
             <Route path="*" element={<Navigate to={"/login"} />} />
         </Routes>
     );
