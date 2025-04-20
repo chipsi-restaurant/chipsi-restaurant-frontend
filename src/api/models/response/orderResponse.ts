9988ada1914ffe7b34922a95508ed1683ac2963d
sinusxd <senya.kolyshkin.gg@gmail.com>
2025-04-20 20:28:26 +0300

    CHIPSI-34 Добавил сущности для работы с order

---
+++ src/api/models/response/orderResponse.ts
@@ -0,0 +1,27 @@
+export interface OrderItemResponse {
+    id: number;
+    name: string;
+    price: number;
+    quantity: number;
+    total: number;
+}
+
+export interface DeliveryResponse {
+    address: string;
+    floor: number;
+    apartmentNumber: number;
+    intercomCode: string;
+    notes: string;
+    status: string;
+}
+
+export interface OrderResponse {
+    id: number;
+    createdAt: string;
+    totalPrice: number;
+    usedBonuses: number;
+    finalPrice: number;
+    status: string;
+    items: OrderItemResponse[];
+    delivery: DeliveryResponse;
+}
---
+++ src/api/services/orderService.ts
@@ -0,0 +1,13 @@
+import {OrderRequest} from "../models/request/orderRequest";
+import {AxiosResponse} from "axios";
+import api from "../api";
+import {OrderResponse} from "../models/response/orderResponse";
+
+export default class OrderService {
+    static async create(request: OrderRequest): Promise<AxiosResponse<any>> {
+        return api.post<AxiosResponse<any>>(`/api/v1/orders`, request)
+    }
+    static async getMyOrders(): Promise<AxiosResponse<OrderResponse[]>> {
+        return api.get<OrderResponse[]>("/api/v1/orders/mine");
+    }
+}
